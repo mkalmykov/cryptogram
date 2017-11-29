e1b485852bb2797e2bd6eda1f2fe2da8b7a42b0c
Maksim Kalmykov <m.n.kalmykov@gmail.com>
2017-11-29 23:30:37 +0400

    Mapper moved to the separate module

--- lib/cryptogram/ciphers/caesar.rb
+++ lib/cryptogram/ciphers/caesar.rb
@@ -1,8 +1,10 @@
+require 'cryptogram/mapper'
+
 module Cryptogram
   module Ciphers
     class Caesar
       def initialize(alphabet:, shift:)
-        @mapper = Mapper.new(alphabet: alphabet, shift: shift)
+        @mapper = ::Cryptogram::Mapper.new(alphabet: alphabet, shift: shift)
       end
 
       def encrypt(string)
@@ -23,32 +25,5 @@ module Cryptogram
         string.each_char.map(&block).join
       end
     end
-  end
-
-  class Mapper
-    def initialize(alphabet:, shift:)
-      @shift = shift
-
-      @shifted_mapper = build_mapper(alphabet)
-      @shifted_swapcased_mapper = build_mapper(alphabet.join.swapcase.chars)
-    end
-
-    def map_by_key(char)
-      shifted_mapper.dig(char) || shifted_swapcased_mapper.dig(char) || char
-    end
-
-    def map_by_value(char)
-      shifted_mapper.key(char) || shifted_swapcased_mapper.key(char) || char
-    end
-
-    private
-
-    def build_mapper(alphabet)
-      shifted_alphabet = alphabet.rotate(shift)
-
-      Hash[alphabet.zip(shifted_alphabet)]
-    end
-
-    attr_reader :shift, :shifted_mapper, :shifted_swapcased_mapper
   end
 end
