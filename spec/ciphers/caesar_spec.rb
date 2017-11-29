32daaa712faac9b8ab92ca305819a54b57b1b4da
Maksim Kalmykov <m.n.kalmykov@gmail.com>
2017-11-29 22:12:28 +0400

    Rubocop config added, offenses fixed

--- spec/ciphers/caesar_spec.rb
+++ spec/ciphers/caesar_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Cryptogram::Ciphers::Caesar do
     let(:encrypted_phrase) { encriptor.encrypt(phrase) }
 
     it 'can encrypt' do
-      expect(encrypted_phrase).to eq expected_encrypted_phrase
+      expect(encrypted_phrase).to eq expected_cryptogram
     end
 
     it 'can decrypt' do
@@ -19,21 +19,21 @@ RSpec.describe Cryptogram::Ciphers::Caesar do
 
   context 'when shift is less than alphabet length' do
     let(:shift) { alphabet.length - 1 }
-    let(:expected_encrypted_phrase) { 'ozbj lx anw vhsg ehud cnydm khptnq itfr' }
+    let(:expected_cryptogram) { 'ozbj lx anw vhsg ehud cnydm khptnq itfr' }
 
     include_context 'with alphabetical chars'
   end
 
   context 'when shift is greater than alphabet length' do
     let(:shift) { alphabet.length + 1 }
-    let(:expected_encrypted_phrase) { 'qbdl nz cpy xjui gjwf epafo mjrvps kvht' }
+    let(:expected_cryptogram) { 'qbdl nz cpy xjui gjwf epafo mjrvps kvht' }
 
     include_context 'with alphabetical chars'
   end
 
   context 'when shift is equal to alphabet length' do
     let(:shift) { alphabet.length }
-    let(:expected_encrypted_phrase) { phrase }
+    let(:expected_cryptogram) { phrase }
 
     include_context 'with alphabetical chars'
   end
